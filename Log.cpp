94a4efb3ac7a1978083885dc97bc7ef259d12352
Allpyg <36484763+Allpyg@users.noreply.github.com>
2019-01-16 18:01:46 +0100

    Petites avancées

--- Log.cpp
+++ Log.cpp
@@ -14,14 +14,33 @@
 
 using namespace std;
 
-//------------------------------------------------------- Includes personnels --
+//------------------------------------------------------- Includes personnels -
+#include "Log.h"
 //------------------------------------------------------------------ CONSTANTES
 
 
 //////////////////////////////////////////////////////////////////////// PUBLIC
 //------------------------------------------------------- Methodes publiques --
 //-------------------------------------------------- Surcharge d'operateurs --
+Log & Log::operator = ( const Log & log )
+{
+	date = log.date;
+	cible = log.cible;
+	refer = log.refer;
+	contenuIndispensable = log.contenuIndispensable;
+}//-- Fin de operator =
+
+
 //---------------------------------------------- Constructeurs - Destructeur --
+Log::Log ( Date laDate, string laCible, string leRefer, bool leContenuIndispensable ) : date(laDate), cible(laCible), refer(leRefer),
+	contenuIndispensable(leContenuIndispensable)
+{
+#ifdef MAP
+	cout << "Construction Log" << endl;
+#endif
+}//--- Fin de Log
+
+
 
 
 ///////////////////////////////////////////////////////////////////////// PRIVE
--- Log.h
+++ Log.h
@@ -12,12 +12,16 @@
 
 ////////////////////////////////////////////////////////// Interfaces utilisees
 #include "Date.h"
+
+#include <string>
+#include <iostream>
 //------------------------------------------------------------------ Constantes
 //----------------------------------------------------------------------- Types
 
 //-----------------------------------------------------------------------------
 //	Role de la classe <Log>
-//
+//		Permet de lire et de stocker les informations sinteressantes du Log 
+//			(pour le TP)
 //-----------------------------------------------------------------------------
 class Log
 {
@@ -26,14 +30,63 @@ class Log
 public :
 	//----------------------------------------------- Methodes publiques --
 	//------------------------------------------- Surcharge d'operateurs --
+	Log & operator = ( const Log & log );
+	// Mode d'emploi :
+	//	Operateur d'affectation de la classe Log.
+	//	log : le Log a copier
+	//	retour : le log copie (*this)
+	// Contrat :
+	//	Aucun contrat.
+
+
+	istream & operator >> ( istream & in );
+	// Mode d'emploi :
+	//	Permet d'affecter les parametres de l'instance Log en lisant un Log
+	//		Apache.
+	//	in : le flux d'entree.
+	//	retour : le flux d'entree.
+	// Contrat :
+	//	Aucun contrat.
+
+
 	//-------------------------------------- Constructeurs - Destructeur --
+	Log ( Date laDate = Date(), string laCible = "", string leRefer = "",
+		bool leContenuIndispensable = false);
+	// Mode d'emploi :
+	//	Constructeur de la classe Log
+	//	laDate : la date du Log
+	//	laCible : la cible du Log
+	//	leRefer : le refer du Log
+	//	leConteneuIndispensable : si le contenu est Image, JS, CSS -> false
+	// Contrat :
+	//	Aucun contrat.
+
+
+	Log ( const Log & log );
+	// Mode d'emploi :
+	//	Constructeur de copie de la classe Log.
+	//	log : le log a copier
+	// Contrat : 
+	//	Aucun contrat.
+
+
+	virtual ~Log ();
+	// Mode d'emploi :
+	//	Destructeur de la classe Log
+	// Contrat :
+	//	Aucun contrat.
 
 
 ///////////////////////////////////////////////////////////////////////// PRIVE
 protected :
 	//----------------------------------------------- Methodes protegees --
 	//----------------------------------------------- Attributs proteges --
-
+	Date date;					// Horaire du Log
+	string cible;				// Cible de la requete du Log
+	string refer;				// Refer (source) du Log
+	bool contenuIndispensable
+		// false : CSS, Image, JS
+		// true sinon
 
 };
 
